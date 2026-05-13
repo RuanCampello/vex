bb197aeb39adc20d7f8cd31cec86dfca13afda94
RuanCampello <ruancampello999@gmail.com>
2026-05-13 07:38:28 -0300

    feat: add basic primitive types

---
+++ src/types.zig
@@ -0,0 +1,49 @@
+const std = @import("std");
+
+pub const PageId = u64;
+pub const Lsn = u64;
+
+/// A mapping-table value
+/// encodes either a live in heap pointer or a disk offset
+///
+/// Layout (64 bits):
+///   bit 63 = 0  ->  memory pointer  (bits 62..0 = address)
+///   bit 63 = 1  ->  disk offset     (bits 62..0 = byte offset in LSS)
+pub const SlotValue = u64;
+
+pub const PhysicalLocation = enum { memory, disk };
+
+pub const INVALID_PAGE_ID = std.math.maxInt(PageId);
+pub const DISK_FLAG: u64 = 1 << 63;
+
+/// Encodes a heap pointer into a slot value
+pub fn encodeMemory(ptr: *anyopaque) SlotValue {
+    const addr = @intFromPtr(ptr);
+    std.debug.assert(addr & DISK_FLAG == 0); // addr cannot touch 63 bit
+
+    return addr;
+}
+
+/// Encodes a disk offset into a slot value
+pub fn encodeDisk(offset: u64) SlotValue {
+    std.debug.assert(offset & DISK_FLAG == 0); // offset cannot touch 63 bit
+    return offset | DISK_FLAG;
+}
+
+/// Decodes a slot value known to be a memory pointer
+/// Crashes if the slot actually encodes a disk offset
+pub fn decodeMemory(slot: SlotValue) *anyopaque {
+    std.debug.assert(slot & DISK_FLAG == 0); // must be a memory slot
+    return @ptrFromInt(slot);
+}
+
+/// Decodes a slot value known to be a disk offset
+/// Crashes if the slot actually encodes a memory pointer
+pub fn decodeDisk(slot: SlotValue) u64 {
+    std.debug.assert(slot & DISK_FLAG != 0); // must be a disk slot
+    return slot & ~DISK_FLAG;
+}
+
+pub fn slotLocation(slot: SlotValue) PhysicalLocation {
+    return if (slot & DISK_FLAG != 0) .disk else .memory;
+}

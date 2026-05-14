1342572e6e07df530cf7f9d66631563b16b6223a
RuanCampello <ruancampello999@gmail.com>
2026-05-14 06:18:41 -0300

    feat: start delta node struct

--- src/page.zig
+++ src/page.zig
@@ -9,7 +9,56 @@ const Lsn = types.Lsn;
 /// Byte comparable owned slice key
 pub const Key = []const u8;
 
-pub const Delta = union(enum) {
+pub const Delta = struct {
+    content: Node,
+    next: ?*Delta,
+
+    pub fn newDelta(alloc: Allocator, kind: DeltaKind) !*Delta {
+        const node = try alloc.create(Delta);
+        node.* = .{ .content = .{ .kind = kind }, .next = null };
+
+        return node;
+    }
+
+    pub fn newPage(alloc: Allocator, page: Page) !*Delta {
+        const node = try alloc.create(Delta);
+        node.* = .{ .content = .{ .page = page }, .next = null };
+
+        return node;
+    }
+
+    pub fn getPage(self: *Delta) *Page {
+        return &self.getNode().content.page;
+    }
+
+    pub fn getNode(self: *Delta) *Delta {
+        var curr: *Delta = self;
+        while (curr.next) |nxt| curr = nxt;
+        std.debug.assert(curr.content == .page); // chain must terminate in a page
+
+        return curr;
+    }
+
+    pub fn len(self: *const Delta) usize {
+        var length: usize = 0;
+        var curr: ?*const Delta = self;
+
+        while (curr) |node| : (curr = node.next) length += 1;
+        return length;
+    }
+};
+
+pub const Page = struct {};
+
+/// A single node in page's delta chain
+/// `next` is non-null for every node except the sentinal that wraps the
+/// base page
+pub const Node = union(enum) {
+    kind: DeltaKind,
+    page: Page,
+};
+
+pub const DeltaKind = union(enum) {
     insert: struct { key: Key, value: []const u8, lsn: Lsn },
     update: struct { key: Key, value: []const u8, lsn: Lsn },
     delete: struct { key: Key, lsn: Lsn },
@@ -39,7 +88,7 @@ pub const Delta = union(enum) {
     /// posted by the flush/eviction path when the page was written to disk
     flush: struct { disk_offset: u64, max_flushed_lsn: Lsn },
 
-    pub fn lsn(self: Delta) ?Lsn {
+    pub fn lsn(self: DeltaKind) ?Lsn {
         return switch (self) {
             .insert => |d| d.lsn,
             .update => |d| d.lsn,
